ec06c5231989561af35ca56e6eae22f11714b82f
Satyajit-tech2006 <satyajitswain2006@gmail.com>
2026-01-28 17:38:12 +0530

    Maintennacne

--- src/app.js
+++ src/app.js
@@ -43,7 +43,6 @@ import userRouter from "./routes/user.route.js";
 // --- Mount Routers ---
 app.use("/api/v1/users", userRouter);
 
-// --- Global Error Handler ---
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
